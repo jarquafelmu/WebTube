2d4e4381ed06075b2143010a131d7c07b8d63038
Sam Terrazas <thechaoselement@gmail.com>
2023-10-11 09:18:03 -0600

    Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,53 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+      dark: [
+        "bg-secondary-dark",
+        "hover:bg-secondary-dark-hover",
+        "text-secondary",
+      ],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
+
+export default function Button({
+  variant,
+  size,
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      // Using twMerge from tailwind-merge to merge the classes from the buttonStyles function with the className prop
+      className={twMerge(buttonStyles({ variant, size, className }), className)}
+    >
+      {children}
+    </button>
+  );
+}
