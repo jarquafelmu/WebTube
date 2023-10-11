f7700a93edad671d6e4b03bb074cafb6eadb7498
Sam Terrazas <thechaoselement@gmail.com>
2023-10-11 09:44:35 -0600

    video grid item

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CategoryPills from "./components/CategoryPills";
 import { categories } from "./data/home";
 import { PageHeader } from "./layouts/PageHeader";
+import VideoGridItem from "./components/VideoGridItem";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -18,6 +19,9 @@ function App() {
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className="grid gap-4 grid-col-[repeat(auto-fill, minmax(300px, 1fr))]">
+            <VideoGridItem />
           </div>
         </div>
       </div>
