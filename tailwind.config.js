6d9af4e73ab6fd4e818a5998295cdf18501cde34
Sam Terrazas <thechaoselement@gmail.com>
2023-10-11 09:17:45 -0600

    configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: {
+          DEFAULT: colors.neutral[100],
+          hover: colors.neutral[300],
+          border: colors.neutral[400],
+          text: colors.neutral[500],
+          dark: colors.neutral[800],
+          ["dark-hover"]: colors.neutral[900],
+        },
+      },
+    },
   },
   plugins: [],
 };
